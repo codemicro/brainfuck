e8e8a7ddcadfb6abad12bb0bf04880da1e9615c6
Tom <tom@tdpain.net>
2021-01-03 23:23:37 +0000

    Add base language definitions

---
+++ internal/def/def.go
@@ -0,0 +1,12 @@
+package def
+
+const (
+	SymbolIncPtr    = byte('>')
+	SymbolDecPtr    = byte('<')
+	SymbolIncVal    = byte('+')
+	SymbolDecVal    = byte('-')
+	SymbolOutput    = byte('.')
+	SymbolInput     = byte(',')
+	SymbolLoopStart = byte('[')
+	SymbolLoopEnd   = byte(']')
+)
